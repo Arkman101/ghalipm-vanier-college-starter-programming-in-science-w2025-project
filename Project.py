5c624cff743cf8dc3b3baf01dbe28c81a786593d
Ghalip Memet <45562923+ghalipm@users.noreply.github.com>
2025-04-08 00:43:24 -0400

    Update Project.py
    Q4 update

--- Project.py
+++ Project.py
@@ -2,6 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scatter_plot import create_scatter_plot
 from square_root_regression import fit_square_root_regression
+from logarithmic_regression import fit_logarithmic_regression
+from surface_plot import create_surface_plot
 import numpy as np
 import os
 
@@ -26,6 +28,12 @@ def fit_square_root_regression(data_file='data.txt'):
     # Do your coding here
     pass
 
+# Q4: Create a 3D plot for the given function z = exp(-0.01*x**2 - 0.01*y**2) over x, y in (-20, 20).
+
+def create_surface_plot():
+     # Do your coding here
+    pass
+    
 
 if __name__ == '__main__':
     unittest.main()
