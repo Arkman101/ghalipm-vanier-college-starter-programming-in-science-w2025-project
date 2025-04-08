3a3d98c5aa3d20c962c5fe8665f703e022c54be6
Ghalip Memet <45562923+ghalipm@users.noreply.github.com>
2025-04-07 23:55:22 -0400

    Update Project.py
    Q1 and Q2 function frames update

--- Project.py
+++ Project.py
@@ -1,1 +1,25 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from scatter_plot import create_scatter_plot
+import numpy as np
+import os
 
+
+# Q1: Create a scatter plot of y = x^2 using data from a CSV file
+
+def create_scatter_plot(csv_file='mathematics.csv'):
+    # Do your coding here
+    return fig
+
+
+# Q2: Logarithmic Regression - Fit a logarithmic regression model to noisy data and plot the result.
+
+def fit_logarithmic_regression(data_file='noisy_data.txt'):
+    # Do your coding here
+    return fig
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
